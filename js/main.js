a5d171eb4b66dfcea52a023816016526b48e5a7a
Maksyuk Vadim <maksyukvadim@gmail.com>
2015-10-26 19:16:17 +0400

    add new function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
-
 var Battleships = function() {
 	var size = 10,
-		 field = initField(size);
+		field = initField(size);
 	var rules = {
 		'1': 4,
 		'2': 3,
@@ -23,32 +22,50 @@ var Battleships = function() {
 		}
 		return res;
 	}
-	  function CheckBorder(x, y) {
-		if (x > 0 && x < 10 && y > 0 && y < 10 ) {
+
+	var CreateTable = function() {
+		var myTable = document.createElement("table");
+		document.body.appendChild(myTable);
+		for (var i = 9; i >= 0; i--) {
+			var tr = document.createElement("tr");
+			myTable.appendChild(tr);
+			for (var j = 0; j < 10; j++) {
+				var td = document.createElement("td");
+				tr.appendChild(td);
+
+				td.id = i + ":" + j;
+				td.style.background = "gray";
+				td.innerHTML = "" + i + ":" + j;
+			}
+		}
+	};
+
+	CreateTable();
+
+	function CheckBorder(x, y) {
+		if (x >= 0 && x < 10 && y >= 0 && y < 10) {
 			return true;
-		} else{
+		} else {
 			return false;
 		}
 	}
 
 	var checkForShips = function(x, y, sizeShip) {
-console.log(sizeShip);
-console.log(sizeShip);
+
 
 		if (x + sizeShip > 10) {
 			alert("корабль выходит за рамки поля вправо");
 		} else {
-			for(var i = 0; i < sizeShip; i++) {
+			for (var i = 0; i < sizeShip; i++) {
 				if (field[y][x + i].ship == true) {
 					alert("нельзя разместить корабль вправо");
 				}
 			}
-
 		}
 		if (x - sizeShip < -1) {
 			alert("корабль выходит за рамки поля влево");
 		} else {
-			for(var i = 0; i < sizeShip; i++) {
+			for (var i = 0; i < sizeShip; i++) {
 				if (field[y][x - i].ship == true) {
 					alert("нельзя разместить корабль ввлево");
 				}
@@ -57,16 +74,16 @@ console.log(sizeShip);
 		if (y + sizeShip > 10) {
 			alert("корабль выходит за рамки поля вверх");
 		} else {
-			for(var i = 0; i < sizeShip; i++) {
+			for (var i = 0; i < sizeShip; i++) {
 				if (field[y + i][x].ship == true) {
 					alert("нельзя разместить корабль вверх");
 				}
 			}
 		}
-		if (y - sizeShip < -1 ) {
+		if (y - sizeShip < -1) {
 			alert("корабль выходит за рамки поля вниз");
 		} else {
-			for(var i = 0; i < sizeShip; i++) {
+			for (var i = 0; i < sizeShip; i++) {
 
 				if (field[y - i][x].ship == true) {
 					alert("нельзя разместить корабль вниз");
@@ -76,33 +93,38 @@ console.log(sizeShip);
 
 
 	}
-       
-	this.putShip = function (x, y) {
+	this.putShip = function(x, y) {
 		// добавить проверку на край поля
 		// также добавить проверку на соседние корабли
 		field[y][x].ship = true;
 	}
-	this.hit = function (x, y) {
+	this.hit = function(x, y) {
 		// добавить проверку на край поля
 		// сделать проверку на рядом стоящий ОДИНАРНЫЙ корабль
 		field[y][x].opened = true;
 	}
 	this.addSips = function(x, y) {
 
-		if (CheckBorder(x, y) == false) {
-			return alert("Вы ввели кординаты которые не попадают в поле");
-		};
-		var sizeShip = +prompt("Введите размер корабля", 1);
-		if(sizeShip > 4 || sizeShip < 1){
-			return alert("Вы ввели неправельный размер корабля");
+			if (CheckBorder(x, y) == false) {
+				return alert("Вы ввели кординаты которые не попадают в поле");
+			};
+			var sizeShip = +prompt("Введите размер корабля", 1);
+			if (sizeShip > 4 || sizeShip < 1) {
+				return alert("Вы ввели неправельный размер корабля");
+			}
+			checkForShips(x, y, sizeShip);
+
 		}
-		console.log(sizeShip);
-		checkForShips(x, y, sizeShip);
+		// добавить метод, который будет добавлять корабли (до 4 палуб)
 
-	}
-	// добавить метод, который будет добавлять корабли (до 4 палуб)
-
-}
+		}
+onclick = function Click (e) {
+			
+			console.log(e.target.id);
+				var tdId = e.target.id;
+				console.log(typeof +tdId[0]);
+				bg.putShip(+tdId[2],+tdId[0]);
+				//console.log(field[tdId[2]][tdId[0]]);
+		}
 
 var bg = new Battleships(10);
-bg.addSips(9,9);
