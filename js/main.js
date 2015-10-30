8abebc19b5c37ef90beaedca6d2cb34df8673d8c
Maksyuk Vadim <maksyukvadim@gmail.com>
2015-10-31 02:42:51 +0400

    add click

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ var Battleships = function() {
 		'3': 2,
 		'4': 1
 	}
+	var masSide = [];
+	var x1,x2,y1,y2;
 
 	function initField(size) {
 		var res = [];
@@ -38,6 +40,9 @@ var Battleships = function() {
 				td.innerHTML = "" + i + ":" + j;
 			}
 		}
+		myTable.onmousedown = myTable.onselectstart = function() {
+  return false;
+}
 	};
 
 	CreateTable();
@@ -49,50 +54,92 @@ var Battleships = function() {
 			return false;
 		}
 	}
-
-	var checkForShips = function(x, y, sizeShip) {
-
-
+	var checkForRight = function(x, y, sizeShip) {
 		if (x + sizeShip > 10) {
 			alert("корабль выходит за рамки поля вправо");
+			return false;
 		} else {
 			for (var i = 0; i < sizeShip; i++) {
 				if (field[y][x + i].ship == true) {
-					alert("нельзя разместить корабль вправо");
+					return false;
 				}
 			}
 		}
+		return true;
+	};
+	var checkForLeft = function(x, y, sizeShip) {
 		if (x - sizeShip < -1) {
 			alert("корабль выходит за рамки поля влево");
+			return false;
 		} else {
 			for (var i = 0; i < sizeShip; i++) {
 				if (field[y][x - i].ship == true) {
-					alert("нельзя разместить корабль ввлево");
+					return false;
 				}
 			}
 		}
+		return true;
+	};
+	var checkForTop = function(x, y, sizeShip) {
 		if (y + sizeShip > 10) {
 			alert("корабль выходит за рамки поля вверх");
+			return false;
 		} else {
 			for (var i = 0; i < sizeShip; i++) {
 				if (field[y + i][x].ship == true) {
-					alert("нельзя разместить корабль вверх");
+					return false;
 				}
 			}
 		}
+		return true;
+	};
+	var checkForBot = function(x, y, sizeShip) {
 		if (y - sizeShip < -1) {
 			alert("корабль выходит за рамки поля вниз");
+			return false;
 		} else {
 			for (var i = 0; i < sizeShip; i++) {
 
 				if (field[y - i][x].ship == true) {
-					alert("нельзя разместить корабль вниз");
+					return false;
 				}
 			}
 		}
-
-
+		return true;
+	};
+	 function sizeShip  (x1, y1, x2, y2) {
+		if (x1==x2) {
+			 
+			alert( Math.abs(y1-y2)+1);
+		}else{
+		alert( Math.abs(x1-x2)+1);
+		}
+		
 	}
+	var side = function(x, y, sizeShip) {
+
+		if(checkForLeft(x, y, sizeShip)){
+			masSide.push("влево");
+		}
+		if(checkForRight(x, y, sizeShip)){
+			masSide.push("вправо");
+		}
+		if(checkForTop(x, y, sizeShip)){
+			masSide.push("вверх");
+		}
+		if(checkForBot(x, y, sizeShip)){
+			masSide.push("вниз");
+		}
+	};
+	var message = function() {
+		for (var i = 0; i < masSide.length; i++) {
+			alert("корабль можно разместить "+ masSide[i]);
+		}
+	};
+
+	this.getShip = function(x, y) {
+		return field[y][x];
+	};
 	this.putShip = function(x, y) {
 		// добавить проверку на край поля
 		// также добавить проверку на соседние корабли
@@ -103,28 +150,43 @@ var Battleships = function() {
 		// сделать проверку на рядом стоящий ОДИНАРНЫЙ корабль
 		field[y][x].opened = true;
 	}
-	this.addSips = function(x, y) {
+	var addSips = function(x, y) {
 
 			if (CheckBorder(x, y) == false) {
 				return alert("Вы ввели кординаты которые не попадают в поле");
 			};
-			var sizeShip = +prompt("Введите размер корабля", 1);
+			var sizeShip = 1;
 			if (sizeShip > 4 || sizeShip < 1) {
 				return alert("Вы ввели неправельный размер корабля");
 			}
-			checkForShips(x, y, sizeShip);
+			//side(x, y, sizeShip);
+		//	message();
+			masSide =[];
 
 		}
 		// добавить метод, который будет добавлять корабли (до 4 палуб)
 
-		}
-onclick = function Click (e) {
-			
-			console.log(e.target.id);
-				var tdId = e.target.id;
-				console.log(typeof +tdId[0]);
-				bg.putShip(+tdId[2],+tdId[0]);
-				//console.log(field[tdId[2]][tdId[0]]);
-		}
+	onmousedown = function a(e) {
+		var tdId1 = e.target.id;
+		console.log(tdId1);
+		x1 =+tdId1[2];
+		y1 =+tdId1[0];
+		addSips(+tdId1[2],+tdId1[0])
+		
+	};
+	onmouseup = function b(e) {
+		var tdId2 = e.target.id;
+		console.log(tdId2);
+		x2 =+tdId2[2];
+		y2 =+tdId2[0];
+		sizeShip(y1,x1,tdId2[0],tdId2[2]);
+		
+	}
+	
+	
 
+
+}
 var bg = new Battleships(10);
+
+
